659da1b98766ed4779521fc5e6d91a71504bcd13
kerstil <kerstilooke@gmail.com>
2025-03-14 15:10:15 +0200

    Update index.php
    lisame pildi

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1> Meie projekt ITK24 gruppiga</h1>
     <p>See leht arvestab meie muutustega (Loe dünaamiline)</p>
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fet.wikipedia.org%2Fwiki%2FKehtna_Kutsehariduskeskus&psig=AOvVaw1qjYMhBcE67rtr6tKsnv6s&ust=1742043966129000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLjNwtHRiYwDFQAAAAAdAAAAABAE">
+    <img src="https://upload.wikimedia.org/wikipedia/et/thumb/7/7f/Kehtna_8297.JPG/1200px-Kehtna_8297.JPG">
   </body>
 </html>
