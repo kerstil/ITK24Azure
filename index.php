bfe905f7d62a7576f287e0fd9eff14c152effde5
kerstil <kerstilooke@gmail.com>
2025-03-14 15:16:27 +0200

    Update index.php
    pilt

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1> Meie projekt ITK24 gruppiga</h1>
     <p>See leht arvestab meie muutustega (Loe dünaamiline)</p>
-    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AOcticons-mark-github.svg&psig=AOvVaw1am8TwVgAo0Xktr1fHElVc&ust=1742044504196000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOiY5tHTiYwDFQAAAAAdAAAAABAE">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg">
   </body>
 </html>
