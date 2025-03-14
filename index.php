8d9782d8e90e6fa9c559cb0b9c2c979b52223843
kerstil <kerstilooke@gmail.com>
2025-03-14 15:15:34 +0200

    Update index.php
    pilt2

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
   <body>
     <h1> Meie projekt ITK24 gruppiga</h1>
     <p>See leht arvestab meie muutustega (Loe dünaamiline)</p>
-    <img src="https://sonumid.ee/wp-content/uploads/2019/07/Kehtna_1-copy.jpg">
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AOcticons-mark-github.svg&psig=AOvVaw1am8TwVgAo0Xktr1fHElVc&ust=1742044504196000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOiY5tHTiYwDFQAAAAAdAAAAABAE">
   </body>
 </html>
